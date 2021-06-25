f32916f858457246f9ba5afa45e4e9b706795e6d
Pierre Laurent <pierre-laurent@outlook.com>
2021-06-25 16:48:46 +0200

    Component introduction

--- __init__.py
+++ __init__.py
@@ -29,3 +29,51 @@ def build(path):
         execfile(path)
     else:
         cmds.warning('The file \'{0}\' doesnt exist.'.format(path))
+
+
+class Component(object):
+
+    component_type_attr_name = 'componentType'
+
+    def __init__(self, folder):
+        if not self.is_one(folder):
+            cmds.error('\'{0}\' is not a valid component folder'.format(folder))
+        self.__folder = folder
+
+    def __str__(self):
+        return self.get_folder()
+
+    def __repr__(self):
+        return self.get_folder()
+
+    @classmethod
+    def is_one(cls, folder):
+        if cmds.objExists('{0}.{1}'.format(folder, cls.component_type_attr_name)):
+            return True
+        return False
+
+    @classmethod
+    def create_folder(cls, dags=None, name=None, parent=None):
+        dags = tuple() if dags is None else [str(dag) for dag in dags]
+        parent = str(parent)
+
+        component_type = cls.__name__.lower()
+        name = component_type if name is None else str(name)
+
+        folder = cmds.group(name=name, empty=True)
+        cmds.addAttr('{0}.{1}'.format(folder, cls.component_type_attr_name), attributeData='string', enumName=component_type)
+
+        if dags:
+            cmds.parent(dags, folder)
+
+        if cmds.objExists(parent):
+            cmds.parent(folder, parent)
+
+        return cls(folder)
+
+    @classmethod
+    def create(cls):
+        raise NotImplementedError
+
+    def get_folder(self):
+        return self.__folder
