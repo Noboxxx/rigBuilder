644f8810da8db3498ad3549ebe9b9bd5c0fbd55b
Pierre Laurent <pierre-laurent@outlook.com>
2022-03-21 16:12:49 +0100

    keep scene name, type and camera position

--- steps/newScene.py
+++ steps/newScene.py
@@ -5,4 +5,19 @@ from maya import cmds
 class NewScene(Step):
 
     def build(self):
+        # old scene name
+        sceneName = cmds.file(q=True, sn=True)
+        sceneType = cmds.file(q=True, type=True)
+        print sceneName, sceneType
+
+        perspMatrix = cmds.xform('persp', q=True, matrix=True)
+
+        # new scene
         cmds.file(new=True, force=True)
+
+        # restore
+        if sceneName:
+            cmds.file(rename=sceneName)
+        cmds.file(type=sceneType[0])
+
+        cmds.xform('persp', matrix=perspMatrix)
